f0a292b6514da684160e1e0ef050d34a40b7a835
Emixham <Maximed.ipod@gmail.com>
2019-11-27 15:45:30 +0100

    ertgyhjk

---
+++ Projet1/Controllers/CafeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Projet1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Projet1.Controllers
+{
+    public class CafeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var offres = new List<MachineACafe>()
+            {
+                new MachineACafe(1, "Mac 1"),
+                new MachineACafe(2, "Mac 2"),
+                new MachineACafe(3, "Mac 3")
+            };
+            return View(offres);
+        }
+    }
+}
---
+++ Projet1/Models/MachineACafe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projet1.Models
+{
+    public class MachineACafe
+    {
+        private string titre;
+        private int id;
+
+        public MachineACafe(int id, string titre)
+        {
+            this.titre = titre;
+            this.id = id;
+        }
+        public int Id => id;
+        public string Titre => titre;
+
+    }
+}
